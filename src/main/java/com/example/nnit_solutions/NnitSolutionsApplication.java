656e3cb912f8c5a8634d35c32f007b17e68cc266
bathulapraka <bathulaprakash406@gmail.com>
2025-12-24 11:32:26 +0530

    first commit

---
+++ src/main/java/com/example/nnit_solutions/NnitSolutionsApplication.java
@@ -0,0 +1,15 @@
+package com.example.nnit_solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnitSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NnitSolutionsApplication.class, args);
+
+        System.out.println("Prakash");
+	}
+
+}
---
+++ src/test/java/com/example/nnit_solutions/NnitSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nnit_solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnitSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
